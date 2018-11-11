d48f106f13dc9b40860977e7a9eb87d053759956
sraju728@gmail.com <sraju728@gmail.com>
2018-11-11 22:39:41 +0530

    Adding global variables

--- AirbagDeploy.ino
+++ AirbagDeploy.ino
@@ -14,3 +14,8 @@ struct
   volatile unsigned int FLAG_ISR_TIMER0:1;
   volatile unsigned int FLAG_ISR_INT0:1;
 }FLAG;
+
+volatile uint16_t counter = 0x00;
+volatile int value;
+volatile int angle = 0;
+Servo servo_test;
