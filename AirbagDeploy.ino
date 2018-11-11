5584424e8c38b423053437f667ce5db94a10ede0
sraju728@gmail.com <sraju728@gmail.com>
2018-11-11 22:38:49 +0530

    Adding global structure

--- AirbagDeploy.ino
+++ AirbagDeploy.ino
@@ -8,3 +8,9 @@
 #define SET_BIT(PORT,BIT) PORT|= (1<<BIT)
 #define CLR_BIT(PORT,BIT) PORT&= ~(1<<BIT)
 #define ADC_PIN 0
+
+struct
+{
+  volatile unsigned int FLAG_ISR_TIMER0:1;
+  volatile unsigned int FLAG_ISR_INT0:1;
+}FLAG;
